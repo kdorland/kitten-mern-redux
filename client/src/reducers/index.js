d1ed2f8d2a4d1251c6b74cb19307797c956a2bc2
Kristian <krdo@eaaa.dk>
2019-11-28 14:12:17 +0100

    Adding some simple tests of the Redux state.

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,25 +1,5 @@
 import { combineReducers } from 'redux';
-
-function kittens(state = [], action) {
-    switch (action.type) {
-        case 'ADD_KITTEN': {
-            // input: action.name
-            const kitten = {
-                id: action.id,
-                name: action.name,
-                hobbies: action.hobbies,
-            };
-            return [...state, kitten];
-        }
-        case 'ADD_HOBBY': {
-            const newKittenState = [...state];
-            const index = newKittenState.findIndex(kitten => kitten.id === action.kittenId);
-            newKittenState[index].hobbies.push(action.hobby);
-            return newKittenState;
-        }
-        default: return state;
-    }
-}
+import { kittens } from './kittens';
 
 export default combineReducers({
     kittens
---
+++ test/kitten.test.js
@@ -0,0 +1,36 @@
+const esmRequire = require('esm')(module);
+const { kittens } = esmRequire('../client/src/reducers/kittens');
+const { addKitten, addHobby } = esmRequire('../client/src/actions');
+
+it('can add kitten', () => {
+    const action = addKitten(1, "Garfield", ['sleeping', 'eating']);
+    const state = kittens([], action);
+    expect(state.length).toBe(1);
+});
+
+it('adds kitten name correctly', () => {
+    const action = addKitten(1, "Garfield", ['sleeping', 'eating']);
+    const state = kittens([], action);
+    expect(state[0].name).toBe("Garfield");
+});
+
+it('adds hobby to kitten', () => {
+    const action = addHobby(1, "Sleeping");
+    const state = kittens([{id: 1, name: "Garfield", hobbies: []}], action);
+    expect(state[0].hobbies.length).toBe(1);
+});
+
+it('adds hobby names correctly to kitten', () => {
+    const action = addHobby(1, "Sleeping");
+    const state = kittens([{id: 1, name: "Garfield", hobbies: []}], action);
+    expect(state[0].hobbies[0]).toBe("Sleeping");
+});
+
+it('adds several hobbies correctly', () => {
+    const action = addHobby(1, "Sleeping");
+    let state = kittens([{id: 1, name: "Garfield", hobbies: []}], action);
+    state = kittens(state, action);
+    state = kittens(state, action);
+    expect(state[0].hobbies.length).toBe(3);
+});
+
